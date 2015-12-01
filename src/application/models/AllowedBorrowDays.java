4090c2131b3d4384ee3d2914e51e333b88260837
cuiqi <areucq@outlook.com>
2015-12-01 11:00:53 -0600

    remove models to applicaton package

--- src/application/models/AllowedBorrowDays.java
+++ src/application/models/AllowedBorrowDays.java
@@ -1,4 +1,4 @@
-package models;
+package application.models;
 
 public enum AllowedBorrowDays {
 	SEVEN_DAYS,
--- src/application/models/CheckoutEntry.java
+++ src/application/models/CheckoutEntry.java
@@ -1,4 +1,4 @@
-package models;
+package application.models;
 
 import java.util.Date;
 
