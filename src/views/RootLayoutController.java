b925c888015374b8964637de8b958da9b4c5529c
宿召阳 <zhaoyang.su@yolu-inc.com>
2015-12-01 11:03:51 -0600

    add RootLayout view and controller

---
+++ src/views/RootLayoutController.java
@@ -0,0 +1,33 @@
+package views;
+
+import application.Main;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import models.Book;
+
+/**
+ * Created by su on 12/1/15.
+ */
+public class RootLayoutController {
+
+    @FXML
+    private MenuBar menubar;
+    private Boolean isAdmin;
+    private Main mainApp;
+
+    public void setMainApp(Main mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    public void initAdminLayout(){
+
+    }
+
+    public void initLibrarianLayout(){
+        ObservableList<Menu> menus = menubar.getMenus();
+
+    }
+
+}
