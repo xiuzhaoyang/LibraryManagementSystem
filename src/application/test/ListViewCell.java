a9c585ea1b8d494bcf84d792206fc873f4a0c4ea
cuiqi <areucq@outlook.com>
2015-12-01 22:37:36 -0600

    add listView example

---
+++ src/application/test/ListViewCell.java
@@ -0,0 +1,18 @@
+package application.test;
+
+import javafx.scene.control.ListCell;
+
+public class ListViewCell  extends ListCell<String>
+{
+    @Override
+    public void updateItem(String string, boolean empty)
+    {
+        super.updateItem(string,empty);
+        if(string != null)
+        {
+            Data data = new Data();
+            data.setInfo(string);
+            setGraphic(data.getBox());
+        }
+    }
+}
